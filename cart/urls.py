05018a5ca2e65c95ef6b0fb3ea1b7c40e37e1e64
unknown <ss.sabana8@gmail.com>
2022-02-24 10:51:24 +0530

    foodntaste

--- cart/urls.py
+++ cart/urls.py
@@ -5,8 +5,6 @@ urlpatterns=[
 
     path('cartDetails',views.cart_details,name="cartDetails"),
     path('add_cart/<int:product_id>/',views.add_cart,name="add_cart"),
-
-
     path('cart_decrement/<int:product_id>/',views.min_cart,name="cart_decrement"),
     path('remove/<int:product_id>/',views.cart_delete,name="remove"),
 
--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.home,name='hm'),
     path('<slug:c_slug>/',views.home,name="prod_cat"),
-
     path('<slug:c_slug>/<slug:product_slug>',views.description,name='description'),
     path('search',views.searching,name="search")
 
